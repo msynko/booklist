7e61809a28f232c3514b970d8a974b3b66d3c76a
msynko <marina.synko@gmail.com>
2018-12-18 13:59:16 -0500

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+
+      format.json do
+        render json: @books.map do |book|
+          {
+            title: book.title,
+            author: book.author,
+            already_read: book.already_read
+          }
+        end
+      end
+
+
+    end
+
+  end
+
 
 end
